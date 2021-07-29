251941fd1beccd970e021abf2cda6ce1671b32a2
Jerell <jerell1998@gmail.com>
2021-07-29 20:21:30 +0100

    vis

--- components/cyto.tsx
+++ components/cyto.tsx
@@ -11,17 +11,33 @@ nodeHtmlLabel(cytoscape) // register extension
 const testNetwork = () => {
 	const sourcePipe = new PipeSeg({
 		diameters: [0.9144],
-		start: { x: 1, y: 1, pressure: 300000, temperature: 350 },
+		start: { x: 50, y: 100, pressure: 300000, temperature: 350 },
 		flowrate: 100,
 	})
-	const branch1 = new PipeSeg({ diameters: [0.9144], start: { x: 4, y: 5 } })
+	const branch1 = new PipeSeg({
+		diameters: [0.9144],
+		start: { x: 150, y: 100 },
+	})
+	const branch1p2 = new PipeSeg({
+		diameters: [0.9144],
+		start: { x: 250, y: 50 },
+	})
+	branch1.setDestination(branch1p2)
 
-	const branch2 = new PipeSeg({ diameters: [0.9144], start: { x: 4, y: 5 } })
+	const branch2 = new PipeSeg({
+		diameters: [0.9144],
+		start: { x: 150, y: 100 },
+	})
 	const branch2p2 = new PipeSeg({
 		diameters: [0.9144],
-		start: { x: 4, y: 5 },
+		start: { x: 250, y: 150 },
 	})
 	branch2.setDestination(branch2p2)
+	const branch2p3 = new PipeSeg({
+		diameters: [0.9144],
+		start: { x: 350, y: 150 },
+	})
+	branch2p2.setDestination(branch2p3)
 
 	const s = new Splitter({
 		source: sourcePipe,
@@ -35,10 +51,13 @@ const generateNetwork = () => {
 	const nextID = () => elements.length
 	const elements = [] as any
 
-	const createNode = (x, y) => {
+	const createNode = (x, y, label) => {
 		const id = nextID()
+		if (label === 'PipeSeg') {
+			label = 'Fake node'
+		}
 		const n = {
-			data: { id: `${id}`, label: `Node ${id}`, icon: 'star_rate' },
+			data: { id: `${id}`, label: `${id}) ${label}`, icon: 'star_rate' },
 			position: { x, y },
 			classes: 'nodeIcon',
 		}
@@ -49,6 +68,7 @@ const generateNetwork = () => {
 	const createEdge = (from, to) => {
 		const e = {
 			data: { source: from, target: to, label: `Pipe from ${from} to ${to}` },
+			classes: 'edge',
 		}
 		elements.push(e)
 		return e
@@ -58,21 +78,59 @@ const generateNetwork = () => {
 
 	const parse = (root) => {
 		while (root.source) root = root.source
-		console.log(root)
+
+		const record = (n) => {
+			console.log(n)
+			const componentType = n.constructor.name
+			const { x, y } = n.properties.start
+
+			const elemAtXY = (xpos = x, ypos = y) =>
+				elements.find(
+					(e) => e.position && e.position.x === xpos && e.position.y === ypos
+				)
+
+			const XYelem = elemAtXY()
+
+			if (!XYelem) {
+				const newNode = createNode(x, y, componentType)
+				if (n.source) {
+					const { x: sx, y: sy } = n.source.properties.start
+					console.log(sx, sy, '!', x, y)
+					const sourceElem = elemAtXY(sx, sy)
+					console.log(sourceElem.data.id, '!!', newNode.data.id)
+					createEdge(sourceElem.data.id, newNode.data.id)
+				}
+			}
+		}
+
+		const preOrderTraverse = (elem, cb) => {
+			if (!elem) return
+			cb(elem)
+
+			if (elem.destinations) {
+				for (const child of elem.destinations) {
+					preOrderTraverse(child, cb)
+				}
+			} else if (elem.destination) {
+				preOrderTraverse(elem.destination, cb)
+			}
+		}
+
+		preOrderTraverse(root, record)
 	}
 
 	parse(net)
 
-	createNode(30, 100)
-	createNode(130, 100) // split
-	createNode(230, 50)
-	createNode(230, 100)
-	createNode(230, 150)
+	// createNode(30, 100)
+	// createNode(130, 100) // split
+	// createNode(230, 50)
+	// createNode(230, 100)
+	// createNode(230, 150)
 
-	createEdge(0, 1)
-	createEdge(1, 2)
-	createEdge(1, 3)
-	createEdge(1, 4)
+	// createEdge(0, 1)
+	// createEdge(1, 2)
+	// createEdge(1, 3)
+	// createEdge(1, 4)
 
 	return elements
 }
@@ -100,8 +158,6 @@ const Cyto = (props) => {
 			},
 		])
 
-		console.log(cy.style())
-
 		// cy.add({
 		// 	group: 'nodes',
 		// 	data: { weight: 75, icon: 'huh' },
@@ -127,7 +183,6 @@ const Cyto = (props) => {
 						height: 30,
 						backgroundColor: '#f78d02',
 						label: `data(label)`,
-						textColor: 'red',
 					},
 				},
 				{
