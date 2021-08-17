abaa49835c9388d0d1bc441fa7f9827eb7f41bb5
Jerell <jerell1998@gmail.com>
2021-08-17 10:27:33 +0100

    inlet class definition

--- public/model/inlet.ts
+++ public/model/inlet.ts
@@ -1,5 +1,5 @@
 import Fluid from './fluid'
-import IElement, { PressureSolution } from './element'
+import IElement, { IPhysicalElement, PressureSolution } from './element'
 import Transport from './transport'
 import { defaultFluidConstructor } from './fluid'
 import {
@@ -12,22 +12,73 @@ import {
 export default class Inlet extends Transport {
 	fluid: Fluid | null
 	destination: IElement | null
+	temperature: number = 10
 
-	constructor(name, physical) {
+	constructor(name: string, physical: IPhysicalElement) {
 		super(name, physical, 'Inlet')
 
 		this.fluid = null
 		this.destination = null
 	}
 
-	async applyInletProperties(pressure, temperature, flowrate) {
+	async applyInletProperties(
+		pressure: number,
+		temperature: number,
+		flowrate: number
+	) {
 		const newFluid = await defaultFluidConstructor(
 			new Pressure(pressure, PressureUnits.Pascal),
 			new Temperature(temperature, TemperatureUnits.Kelvin),
 			flowrate
 		)
 
-		return this.process(newFluid)
+		this.fluid = newFluid
+
+		return this.process(this.fluid)
+	}
+
+	async searchInletPressure() {
+		const lowLimit = new Pressure(5, PressureUnits.Bara)
+		const highLimit = new Pressure(150, PressureUnits.Bara)
+
+		let low = lowLimit.pascal
+		let high = highLimit.pascal
+		let mid = 0
+
+		const stepSize = 0.001
+		let guesses = 0
+		const maxGuesses = 25
+
+		let pressureSolution = PressureSolution.Low
+
+		this.applyInletProperties(1, 2, 3)
+
+		if (!this.fluid) {
+			throw new Error(`Inlet has no fluid`)
+		}
+
+		while (pressureSolution !== PressureSolution.Ok) {
+			if (guesses++ > maxGuesses) {
+				console.log(`max guesses (${maxGuesses}) reached`)
+				break
+			}
+
+			mid = (low + high) / 2
+
+			pressureSolution = await this.applyInletProperties(
+				mid,
+				this.temperature,
+				this.fluid.flowrate
+			)
+
+			if (pressureSolution === PressureSolution.Low) {
+				high = mid - stepSize
+			} else if (pressureSolution === PressureSolution.High) {
+				low = mid + stepSize
+			}
+		}
+
+		return { pressure: mid, pressureSolution }
 	}
 
 	setDestination(dest: IElement) {
