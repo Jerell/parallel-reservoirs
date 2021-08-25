2c865d0af11bec2256f3b5d46c4694e406a0a0fc
Jerell <jerell1998@gmail.com>
2021-08-25 16:35:22 +0100

    map expansion and icons

---
+++ components/buttons/expansionButton.tsx
@@ -0,0 +1,30 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronUp, faChevronDown } from '@fortawesome/free-solid-svg-icons'
+
+interface IButtonProps {
+	fn?: (e) => void
+	expanded?: boolean
+}
+
+const ExpansionButton = ({ fn, expanded = true }: IButtonProps) => {
+	function handleClick(e) {
+		e.persist()
+		e.preventDefault()
+		if (fn) {
+			fn(e)
+		}
+	}
+	return (
+		<button
+			className='flex flex-row text-pace-grey bg-white hover:bg-gray-100 w-20 justify-center items-center focus:ring-green-500'
+			onClick={handleClick}
+		>
+			<FontAwesomeIcon
+				icon={expanded ? faChevronUp : faChevronDown}
+				className='w-3 flex'
+			/>
+		</button>
+	)
+}
+
+export default ExpansionButton
--- components/dashboard/map.tsx
+++ components/dashboard/map.tsx
@@ -1,13 +1,27 @@
 import MapSvg from '@/public/images/map.svg'
 import Heading from '@/components/heading'
+import ExpansionButton from '@/components/buttons/expansionButton'
+import { useState } from 'react'
 
-const Map = () => (
-	<>
-		<Heading level={2}>Network map</Heading>
-		<div className='mx-8'>
-			<MapSvg width={700} height={400} />
-		</div>
-	</>
-)
+const Map = () => {
+	const [open, setOpenState] = useState(true)
+	function toggleExpand(e) {
+		setOpenState(!open)
+	}
+
+	return (
+		<>
+			<div className='flex flex-row'>
+				<Heading level={2}>Network map</Heading>
+				<ExpansionButton expanded={open} fn={toggleExpand} />
+			</div>
+			{open ? (
+				<div className='mx-8'>
+					<MapSvg width={700} height={400} />
+				</div>
+			) : null}
+		</>
+	)
+}
 
 export default Map
