761d7e2294ed8eaecff1011c5255fbce0600668b
Jerell <jerell1998@gmail.com>
2021-09-08 00:08:23 +0100

    basic api endpoint

--- pages/api/snapshot.ts
+++ pages/api/snapshot.ts
@@ -1,19 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import path from 'path';
-import { Diverging } from 'ccs-sim';
+import { Parser } from 'ccs-sim';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
 	const dir = path.resolve('./public');
 
-	const parser = new Diverging.Parser();
+	const parser = new Parser();
 	parser.readFile(path.join(dir, 'whole.yml'));
 	await parser.build();
 	const keyPoints = parser.keyPoints;
 
 	res.status(200).json({
 		keyPoints: keyPoints.reduce((acc, point) => {
-			console.log(point);
-			acc[point.name] = 0;
+			acc[point.name] = point.type;
 			return acc;
 		}, {}),
 	});
