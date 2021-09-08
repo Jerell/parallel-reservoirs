3f08d6501e68b63899d1c5d05ef8093b40e504a4
Jerell <jerell1998@gmail.com>
2021-09-08 12:22:07 +0100

    login

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,28 @@ import Map from '@/components/dashboard/map';
 import DataTable from '@/components/dashboard/data/dataTable';
 import Graphs from '@/components/dashboard/data/graphs';
 import { useState } from 'react';
+import { useSession, signIn, signOut } from 'next-auth/client';
+import SignedOutBanner from '@/components/signedOutBanner';
 
 const Index = () => {
+	const [session, loading] = useSession();
+
 	const [hoverColumn, setHoverColumn] = useState(0);
+
+	if (!session) {
+		return (
+			<Page fullWidth noPadding>
+				<Heading level={1}>Unauthorized</Heading>
+
+				<SignedOutBanner />
+			</Page>
+		);
+	}
 
 	return (
 		<Page fullWidth noPadding>
 			<Heading level={1}>Project name</Heading>
+
 			<Map hoverColumn={hoverColumn} setHoverColumn={setHoverColumn} />
 			<DataTable hoverColumn={hoverColumn} setHoverColumn={setHoverColumn} />
 			<Graphs hoverColumn={hoverColumn} setHoverColumn={setHoverColumn} />
