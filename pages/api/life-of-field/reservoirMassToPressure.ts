563e87c02a2585b239399bfcbbcad4f846e2176a
Jerell <jerell1998@gmail.com>
2021-09-09 13:59:13 +0100

    rename

--- pages/api/life-of-field/reservoirMassToPressure.ts
+++ pages/api/life-of-field/reservoirMassToPressure.ts
@@ -1,37 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { Reservoir } from 'ccs-sim';
-import {
-	Pressure,
-	PressureUnits,
-	Temperature,
-	TemperatureUnits,
-	Flowrate,
-	FlowrateUnits,
-} from 'physical-quantities';
 import protect from '@/public/utils/api/protect';
-import sum from '@/public/utils/sum';
 import checkSubset from '@/public/utils/checkSubset';
+import massToPressure from '@/public/utils/api/massToPressure';
 
-const genericResPressureFunc = (coefs: number[]) => (mass: number) => {
-	return new Pressure(
-		sum(coefs.map((coef, i) => coef * mass ** i)),
-		PressureUnits.Bara
-	);
-};
-
-const massToPressure = {
-	HM: genericResPressureFunc([
-		4.23479637, 4.03128187e-9, -8.53661996e-20, 8.51195441e-31, -3.04373551e-42,
-	]),
-	HN: genericResPressureFunc([
-		4.3772189, 5.58185243e-9, -1.67965284e-19, 2.31321814e-30, -1.09337098e-41,
-	]),
-	LX: genericResPressureFunc([
-		6.65789476, 4.89875978e-9, -1.20961397e-19, 1.31958662e-30, -4.03678013e-42,
-	]),
-};
-
-function validateMassRequest(
+function validateRequest(
 	body: {
 		HM?: number;
 		HN?: number;
@@ -66,8 +38,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 	}
 
 	const body = JSON.parse(req.body);
-
-	validateMassRequest(body, res);
+	validateRequest(body, res);
 	if (res.statusCode === 400) return;
 
 	const pressures = Object.entries(body).reduce((acc, [key, value]) => {
